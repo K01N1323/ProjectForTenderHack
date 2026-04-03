494aa544e25a6e4ebfe6c0d7eefb8ace381f332c
Носов Данил <ddanil00111@gmail.com>
2026-04-03 22:37:00 +0300

    Add semantic search layer and fastText tooling

--- src/tenderhack/__init__.py
+++ src/tenderhack/__init__.py
@@ -1,8 +1,10 @@
 from .personalization import PersonalizationService, build_customer_profile, rerank_offers, rerank_ste
+from .semantic import SemanticExpander
 from .search import SearchService, search_ste
 
 __all__ = [
     "PersonalizationService",
+    "SemanticExpander",
     "SearchService",
     "build_customer_profile",
     "rerank_offers",
